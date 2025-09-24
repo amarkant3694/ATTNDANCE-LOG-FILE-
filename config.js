ecb1c4e658f9363165ebb6c1698d13bfaa1bef3e
Amarkant <153291825+amarkant3694@users.noreply.github.com>
2025-09-24 23:38:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    TELEGRAM_BOT_TOKEN: '7657384442:AAE6_uWcr1gSsSRRXFTdXERGCLgAG--5snw',
+    TELEGRAM_BOT_TOKEN: '7657384442:AAE6_uWcr1gSsSRRXFTdXERGCxdhgdhsjhrfjfctLgAG--5snw',
     TELEGRAM_CHAT_ID: 'YOUR_CHAT_ID', // Add your chat ID here
     PORT: 3000
+
 };
